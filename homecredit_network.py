18e0a7eb1c42386a1653f44a7bd6b81476f258f2
Sarang Zambare <Wolf-Pack@Sarangs-MacBook-Air.local>
2018-11-01 18:13:49 -0700

    Drafted the model with 2 layers

--- homecredit_network.py
+++ homecredit_network.py
@@ -49,7 +49,46 @@ def prepare_data(csv_filename):
     
     y_test = df_test.iloc[:,1]
     
+
+# Cannot be executed yet, the below arguments are only dummies. 
     
+model = models.Sequential()
+model.add(layers.Dense(16,activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
+
+history = model.fit(partial_x_train,partial_y_train,epochs=20  ,batch_size=512,validation_data=(x_val,y_val))
+
+history_dict = history.history
+
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1,len(loss)+1)
+
+#results = model.evaluate(x_test,y_test)
+
+# print(results)
+
+plt.plot(epochs,loss,'bo',label='Training Loss')
+plt.plot(epochs,val_loss,'b',label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+#print(vectorize(train_data))
+
+
+
+
+#print(train_data[0])
+
+# model = model.Sequential()
+# model.add(layers.Dense(16, activation='relu',input_shape=(10000,)))
+# model.add(layers.Dense(16, activation='relu'))
+# model.add(layers.Dense(1,activation='sigmoid'))
     
     
     
